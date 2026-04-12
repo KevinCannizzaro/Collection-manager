943fca08b46a5137b4070e7cbe2c94bf278b96c3
Kevin Cannizzaro <84503315+KevinCannizzaro@users.noreply.github.com>
2026-04-12 19:24:27 +0200

    Add files via upload

---
+++ src/app/app.ts
@@ -0,0 +1,30 @@
+import { Component, signal } from '@angular/core';
+import { CollectionItemCard } from './components/collection-item-card/collection-item-card';
+import { CollectionItem } from './models/collection-item';
+import { SearchBar } from './components/search-bar/search-bar';
+
+@Component({
+  selector: 'app-root',
+  imports: [CollectionItemCard, SearchBar],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+
+export class App {
+
+  count = 0;
+
+  coin! : CollectionItem;
+  linx! : CollectionItem;
+
+  constructor() {
+    this.coin = new CollectionItem();
+    this.coin.name = 'Pièces de 1972';
+    this.coin.description = 'Pièce obtenu dans les jeux vidéos';
+    this.coin.rarity = 'Commune';
+    this.coin.image = 'img/coin1.png';
+    this.coin.price = 170;
+
+    this.linx = new CollectionItem();
+  }
+}
---
+++ src/app/components/collection-item-card/collection-item-card.ts
@@ -0,0 +1,13 @@
+import { Component, input } from '@angular/core';
+import { CollectionItem } from '../../models/collection-item';
+
+@Component({
+  selector: 'app-collection-item-card',
+  imports: [],
+  templateUrl: './collection-item-card.html',
+  styleUrl: './collection-item-card.scss',
+})
+export class CollectionItemCard {
+
+  item = input.required<CollectionItem>();
+}
